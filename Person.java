f0772c3a3177b525c4370d1a51c13212bbecbf53
anduser <Stas_ya92@mail.ru>
2023-01-24 18:24:21 +0300

    first

---
+++ Person.java
@@ -0,0 +1,41 @@
+package org.example;
+
+package Menu;
+
+public class Person {
+
+    //Атрибуты класса
+    private String name = "";
+    private String surname = "";
+    private String year = "";
+
+    private final int ALLFIELDS = 3;
+
+    //Конструктор
+    public Person() {
+    }
+
+    //Методы
+    public String printInfo() {
+        String printout = "";
+
+        for (int i = 0; i < ALLFIELDS; i++) {
+            printout = String.format("Name: %s \nSurname: %s \nYear: %s \n", name, surname, year);
+        }
+        return printout;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+
+}
